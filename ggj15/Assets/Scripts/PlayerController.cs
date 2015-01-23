0c39e07690733e5f61554639d65d4217cc7ad249
Jopz Agcaoili <jopz.agcaoili@gmail.com>
2015-01-24 02:12:16 +0800

    Temp panel added

---
+++ ggj15/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	private static PlayerController m_instance;
+	public static PlayerController Instance { get { return m_instance; } }
+
+	private void Awake()
+	{
+		m_instance = this;
+	}
+
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			Debug.Log(">>>>>>");
+			float x = transform.position.x + (2.0f * Time.deltaTime);
+			transform.position = new Vector2( x, transform.position.y);
+		}
+	}
+
+	private void OnDestroy()
+	{
+		m_instance = null;
+	}
+}
