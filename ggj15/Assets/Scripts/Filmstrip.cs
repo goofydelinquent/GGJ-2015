0721c23c67a2162949f4bfe1511adb80150ae88d
patrick <carl@secret6.com>
2015-01-24 13:37:27 +0800

    Revised Camera Follow Added Filmstrip

---
+++ ggj15/Assets/Scripts/Filmstrip.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Filmstrip : MonoBehaviour
+{
+	private bool m_bCheckForPlayer = false;
+
+	void Update()
+	{
+		if( !m_bCheckForPlayer ) { return; }
+
+		if( PlayerController.Instance.transform.position.x > transform.position.x )
+		{
+			m_bCheckForPlayer = false;
+
+			PanelManager.Instance.IncrementCurrentIndex();
+		}
+	}
+
+	void OnBecameVisible()
+	{
+		m_bCheckForPlayer = true;
+	}
+}
