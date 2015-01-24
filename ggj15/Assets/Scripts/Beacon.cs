fc91f5fed60e71daefd936eca17077e11c635d49
Jopz Agcaoili <jopz.agcaoili@gmail.com>
2015-01-24 23:00:37 +0800

    Trigger now returning string for calling memory sprites

--- ggj15/Assets/Scripts/Beacon.cs
+++ ggj15/Assets/Scripts/Beacon.cs
@@ -6,7 +6,11 @@ public class Beacon : MonoBehaviour {
 
 	public GameObject m_beacon;
 	public Transform m_position;
-	public List<Transform> m_positions = new List<Transform>();
+
+	//public List<int> m_memory = new List<int>();
+	public string[] m_memoryName;
+	private List<Transform> m_positions = new List<Transform>();
+	private string m_memoryAddress = "";
 
 	// Use this for initialization
 	void Start () 
@@ -18,12 +22,15 @@ public class Beacon : MonoBehaviour {
 				m_positions.Add(child);
 			}
 		}
+
+		m_memoryAddress = m_memoryName[Random.Range(0, m_memoryName.Length)];
+
 		m_beacon.transform.SetParent (m_positions [Random.Range (0, m_positions.Count)]);
 		m_beacon.transform.localPosition = Vector3.zero;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public string MemoryFileAddress
+	{
+		get { return m_memoryAddress; }
 	}
 }
