b83b5023386d8385e0fb4c1b81984e2e73c14e09
Richard Locsin <goofydelinquent@gmail.com>
2015-01-25 08:53:58 +0800

    Added SceneRestarter & fixed missing link on title screen particle.

---
+++ ggj15/Assets/Scripts/SceneRestarter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneRestarter : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		if ( Input.GetKey( KeyCode.Backspace ) || Input.GetKey( KeyCode.Backslash ) ) {
+			Application.LoadLevel( 0 );
+		}
+	
+	}
+}
