2dfe928d69344da93fdfc355ecba765420e3282a
Patrick Alain Carlos <pat@secret6.com>
2015-01-26 14:40:10 +0800

    Fading Quote

---
+++ ggj15/Assets/Scripts/Bubble.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bubble : MonoBehaviour
+{	
+	[SerializeField] private TextMesh m_text;
+	[SerializeField] private SpriteRenderer m_spriteBg;
+	
+	private bool m_bActive = false;
+
+	public void Awake()
+	{
+		//TweenFade( 0.0f );
+
+		//iTween.Init( gameObject );
+	}
+	/*
+	public void Fade( bool p_bIn )
+	{
+		if( p_bIn == m_bActive ) { return; }
+		m_bActive = p_bIn;
+
+		if( m_bActive ) {
+			iTween.ValueTo( gameObject, iTween.Hash(
+				"from", 0f,
+				"to", 1f,
+				"time", 1.0f,
+				"onupdatetarget", gameObject,
+				"onupdate", "TweenFade",
+				"easetype", iTween.EaseType.easeOutCubic
+				));
+		}
+		else {
+			iTween.ValueTo( gameObject, iTween.Hash(
+				"from", 1f,
+				"to", 0f,
+				"time", 1.0f,
+				"onupdatetarget", gameObject,
+				"onupdate", "TweenFade",
+				"easetype", iTween.EaseType.easeOutCubic
+				));
+		}
+	}*/
+
+	public void TweenFade( float p_value )
+	{
+		Color color = new Color( 1, 1, 1, p_value );
+		renderer.material.color = color;
+		m_spriteBg.color = color;
+	}
+}
--- ggj15/Assets/Scripts/Quote.cs
+++ ggj15/Assets/Scripts/Quote.cs
@@ -5,11 +5,17 @@ public class Quote : MonoBehaviour
 {	
 	private TextMesh m_text;
 
+	private Color m_color;
+
 	public void Awake()
 	{
 		m_text = GetComponent<TextMesh>();
 
 		m_text.text = "";
+
+		m_color = renderer.material.color;
+
+		TweenFade( 0.0f );
 
 		iTween.Init( gameObject );
 	}
@@ -18,9 +24,18 @@ public class Quote : MonoBehaviour
 	{
 		m_text.text = RandomTextPool.GetRandomText();
 
-		//iTween.FadeTo( gameObject, 0, 0 );
-		//iTween.FadeTo( gameObject, iTween.Hash( "alpha", 1, "time", 0.35f, "easeType", iTween.EaseType.easeInOutQuad ) );
+		iTween.ValueTo( gameObject, iTween.Hash(
+			"from", 0f,
+			"to", 1f,
+			"time", 1.0f,
+			"onupdatetarget", gameObject,
+			"onupdate", "TweenFade",
+			"easetype", iTween.EaseType.easeOutCubic
+			));
 	}
 
-
+	public void TweenFade( float p_value )
+	{
+		renderer.material.color = new Color( m_color.r, m_color.g, m_color.b, p_value );
+	}
 }
